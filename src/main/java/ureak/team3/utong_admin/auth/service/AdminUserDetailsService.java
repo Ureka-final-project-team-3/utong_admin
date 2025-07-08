8e8d0d9cbd68027176da44982dd60167a83301d1
son <cliff615@naver.com>
2025-07-08 23:15:40 +0900

    feat : spring security만 적용한 기본 auth 구현

---
+++ src/main/java/ureak/team3/utong_admin/auth/service/AdminUserDetailsService.java
@@ -0,0 +1,27 @@
+package ureak.team3.utong_admin.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import ureak.team3.utong_admin.auth.entity.Admin;
+import ureak.team3.utong_admin.auth.repository.AdminRepository;
+
+@Service
+public class AdminUserDetailsService implements UserDetailsService {
+
+    private final AdminRepository adminRepository;
+
+    public AdminUserDetailsService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Admin not found: " + email));
+        
+        return admin;
+    }
+}
